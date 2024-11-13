b3c248a614f673e0e503805f3c3c5509fbddcd05
h4fef <afefhassani00@gmail.com>
2024-11-13 11:44:53 +0100

    ok

--- todo/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:todo/views/app_logo_init/app_logo_init.dart';
-
-void main() {
-  runApp(const MaterialApp(
-    title: 'Everything Tasks',
-    home: AppLogoInit(),
-  ));
-}
